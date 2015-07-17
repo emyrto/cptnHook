d29bb8727974e128d902d74db2a31b1a317701ce
emyrto <emyrto@gmail.com>
2015-07-17 17:54:03 +0200

    Update Compile_and_Run_Test.sh

--- Compile_and_Run_Test.sh
+++ Compile_and_Run_Test.sh
@@ -1,5 +1,7 @@
+#! /usr/bin/env sh
+
 # Compile and Run a Test 
-cd ~/cptnHook/Tests
+cd Tests
 g++ -std=c++11 -o simpleFunc simpleFunc.cpp
 cd ../bin
 $PINDIR/pin -t libcptnHook.so -- ../Tests/simpleFunc
