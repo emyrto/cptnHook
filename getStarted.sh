a3d0f4ab282da4b8a60486f9ff55a30d9dad53d5
emyrto <emyrto@gmail.com>
2015-07-27 12:49:42 +0200

    Update getStarted.sh

--- getStarted.sh
+++ getStarted.sh
@@ -3,7 +3,7 @@
 #	Reminder:
 # You need to install ROOT
 #	go here: https://root.cern.ch/drupal/
-#	for further documentation
+#	for further documentation.
 
 echo $PINDIR
 
@@ -28,18 +28,14 @@ python interceptorGen.py > interceptorGen.h
 
 # Compile the PIN tool
 echo Compiling cptnHook
-CPPFLAGS="-DBIGARRAY_MULTIPLIER=1 -Wall -Werror -Wno-unknown-pragmas -fno-stack-protector -Wno-unused-variable -DTARGET_IA32E -DHOST_IA32E -fPIC -DTARGET_LINUX -fomit-frame-pointer -fno-strict-aliasing"
-CPPINCLUDES="-I"$PINDIR"/source/tools/InstLib -I"$PINDIR"/source/include/pin/ -I"$PINDIR"/source/include/pin/gen -I"$PINDIR"/extras/components/include -I"$PINDIR"/extras/xed-intel64/include -I"$PINDIR"/source/tools/InstLib"
-ROOTFLAGS="`root-config --cflags --libs`"
 PINHOOKNAME="cptnHook"
-g++ $CPPFLAGS $CPPINCLUDES $ROOTFLAGS -O3 -c -o "$PINHOOKNAME".o "$PINHOOKNAME".cpp -std=c++11
+g++ -DBIGARRAY_MULTIPLIER=1 -Wall -Werror -Wno-unknown-pragmas -fno-stack-protector -Wno-unused-variable -DTARGET_IA32E -DHOST_IA32E -fPIC -DTARGET_LINUX  -I$PINDIR/source/include/pin -I$PINDIR/source/include/pin/gen -I$PINDIR/extras/components/include -I$PINDIR/extras/xed-intel64/include -I$PINDIR/source/tools/InstLib -O3 `root-config --cflags --libs` -fomit-frame-pointer -fno-strict-aliasing -L$ROOTSYS -c -o cptnHook.o cptnHook.cpp -std=c++11
 
 #-L$ROOTSYS
 mv "$PINHOOKNAME".o ../bin
 cd ../bin
 
 # Make a shared library
-CPPLDFLAGS="-shared -Wl,--hash-style=sysv -Wl,-Bsymbolic -Wl,--version-script=$PINDIR/source/include/pin/pintool.ver -L$PINDIR/intel64/lib -L$PINDIR/intel64/lib-ext -L$PINDIR/intel64/runtime/glibc -L$PINDIR/extras/xed-intel64/lib -lpin -lxed -lpindwarf -ldl"
-g++ $CPPLDFLAGS  -o libcptnHook.so "$PINHOOKNAME".o  $ROOTFLAGS
-
-echo Compilation ended. Remember to set the PINDIR env variable to $PINDIR
+g++ -shared -Wl,--hash-style=sysv -Wl,-Bsymbolic -Wl,--version-script=$PINDIR/source/include/pin/pintool.ver    -o libcptnHook.so cptnHook.o  -L$PINDIR/intel64/lib -L$PINDIR/intel64/lib-ext -L$PINDIR/intel64/runtime/glibc -L$PINDIR/extras/xed-intel64/lib -lpin -lxed -lpindwarf -ldl -L$ROOTSYS `root-config --cflags --libs`
+echo Compilation ended. 
+#Remember to set the PINDIR env variable to $PINDIR
